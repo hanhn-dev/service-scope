ac582ad369011c7bafad0a3d7a9f9b403c57750a
Hanh Nguyen <hanh.nguyen@employgroup.com.au>
2019-05-13 15:09:49 +0700

    Initial Project'

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceScope.Models;
+using ServiceScope.Services;
+
+namespace ServiceScope.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel(
+            OperationService operationService,
+            IOperationTransient transientOperation,
+            IOperationScoped scopedOperation,
+            IOperationSingleton singletonOperation,
+            IOperationSingletonInstance singletonInstanceOperation)
+        {
+            OperationService = operationService;
+            TransientOperation = transientOperation;
+            ScopedOperation = scopedOperation;
+            SingletonOperation = singletonOperation;
+            SingletonInstanceOperation = singletonInstanceOperation;
+        }
+
+        public OperationService OperationService { get; }
+        public IOperationTransient TransientOperation { get; }
+        public IOperationScoped ScopedOperation { get; }
+        public IOperationSingleton SingletonOperation { get; }
+        public IOperationSingletonInstance SingletonInstanceOperation { get; }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
